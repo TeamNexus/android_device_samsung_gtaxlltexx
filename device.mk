f61670aaeb2de069d26b264039433f0f9397a368
Lukas0610 <mail@lukasberger.at>
2017-10-09 17:32:55 +0200

    gtaxlltexx: properly include dalvik/hwui property overrides

--- device.mk
+++ device.mk
@@ -22,6 +22,24 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Inherit from gtaxl-common
 $(call inherit-product, device/samsung/gtaxl-common/gtaxl-common.mk)
 
-#Interherit from dalvic mem allocation overrides
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-2048-dalvik-heap.mk)
+# Inerherit from native memory-overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
  
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
